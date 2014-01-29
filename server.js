8a172f0ec3d4eb171c06c6fe1ac0b40fe14b19af
NuXareon <millas@gmail.com>
2014-01-29 12:42:01 +0100

    mes pr0ves

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ io.set('log level',1);
 
 io.sockets.on('connection', function(socket) {
 	console.log('A socket has conected');
+
+	socket.on('sendMessage', function (data) {
+		io.sockets.emit('printMessage',{username:data.username,message:data.message});
+	})
 });
